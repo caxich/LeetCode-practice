922b0915bcb09ea729dfdde4bb3698c1bd356733
caxich <746135024@qq.com>
2020-02-14 23:57:56 +0800

    0214

---
+++ leet1/509. 斐波那契数/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leet1._509._斐波那契数
+{
+    public class Solution
+    {
+        public int Fib(int N)
+        {
+            var result = 0;
+            if (N == 0)
+                return 0;
+            if (N == 1)
+                return 1;
+            var temp = 1;
+            var second = 0;
+            var i = 0;
+            while (N >= i)
+            {
+                if (i == 0)
+                {
+                    second = 0;
+                    result = 0;
+                    temp = 0;
+                }
+                if (i == 1)
+                {
+                    second = 0;
+                    result = 1;
+                    temp = 0;
+                }
+                second = result;
+                result += temp;
+                temp = second;
+                i++;
+            }
+            return result;
+        }
+    }
+}
---
+++ leet1/961. 重复 N 次的元素/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace leet1._961._重复_N_次的元素
+{
+    public class Solution
+    {
+        public int RepeatedNTimes(int[] A)
+        {
+            var dicts = new Dictionary<int, int>();
+            var temp = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (!dicts.ContainsKey(A[i]))
+                {
+                    dicts.Add(A[i], 1);
+                }
+                else
+                {
+                    temp = dicts[A[i]];
+                    if (temp >= A.Length / 2)
+                    {
+                        return A[i];
+                    }
+                    else
+                    {
+                        dicts[A[i]] = ++temp;
+                    }
+                }
+            }
+            return dicts.Where(x => x.Value >= A.Length / 2).Any() ? dicts.Where(x => x.Value >= A.Length / 2).FirstOrDefault().Key : 0;
+        }
+    }
+}
